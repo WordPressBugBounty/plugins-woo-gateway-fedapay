51a22b467e1a9e5d835cd13ff4b76b6ccbfc430c
Sean Murphy <sean@iamseanmurphy.com>
2025-01-03 22:18:09 +0000

    0.3.7

---
+++ woo-gateway-fedapay/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-html-entities', 'wp-i18n'), 'version' => 'dc6cb8ac3e31b727f519');
--- woo-gateway-fedapay/woo-gateway-fedapay.php
+++ woo-gateway-fedapay/woo-gateway-fedapay.php
@@ -6,14 +6,14 @@
  * Author: Fedapay
  * Author URI: https://fedapay.com/
  * Requires at least: 4.4
- * Tested up to: 5.8
+ * Tested up to: 6.4.3
  * WC requires at least: 2.6
- * WC tested up to: 5.5.2
+ * WC tested up to: 9.4
  * License: GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain: woo-gateway-fedapay
  * Domain Path: /languages
- * Version: 0.3.4
+ * Version: 0.3.7
  *
  */
 
@@ -21,7 +21,8 @@ if (! defined('ABSPATH') ) {
     exit;
 }
 
-define( 'WC_FEDAPAY_GATEWAY_VERSION', '0.3.4' );
+define( 'WC_FEDAPAY_GATEWAY_VERSION', '0.3.7' );
+define( 'WC_FEDAPAY_PLUGIN_FILE_PATH', plugin_dir_path( __FILE__ ) );
 
 function wc_fedapay_gateway() {
     static $plugin;
